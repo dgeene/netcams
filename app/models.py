45fd965339306bc5872b22183624116ec9052832
dgeene <david.geene@gmail.com>
2016-08-06 13:14:52 -0400

    fix relational issues

--- app/models.py
+++ app/models.py
@@ -4,7 +4,15 @@ from app import db
 # http://docs.sqlalchemy.org/en/rel_1_0/orm/basic_relationships.html#many-to-many
 #
 
+# address the many to many with a bridge table
+camera_categories = db.Table('camera_categories', db.Model.metadata,
+    db.Column('camera_id', db.Integer, db.ForeignKey('camera.id')),
+    db.Column('category_id', db.Integer, db.ForeignKey('category.id'))
+)
+
+
 class Category(db.Model):
+    __tablename__ = 'category'
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(20), index=True)
 
@@ -12,6 +20,7 @@ class Category(db.Model):
         return '<Category %r>' % (self.category_name)
 
 class CameraType(db.Model):
+    __tablename__ = 'camera_type'
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(32), index=True)
 
@@ -20,6 +29,7 @@ class CameraType(db.Model):
 
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -27,12 +37,14 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
-class Cameras(db.Model):
+class Camera(db.Model):
+    __tablename__ = 'camera'
     id = db.Column(db.Integer, primary_key=True)
     camera_name = db.Column(db.String(255), index=True)
     url = db.Column(db.String(255))
-    type = db.relationship('CameraType', backref='camera_type', lazy='dynamic')
-    categories = db.relationship('Category', backref='camera_categories', lazy='dynamic')
+    type_id = db.Column(db.Integer, db.ForeignKey('camera_type.id'))
+    type = db.relationship('CameraType', backref='camera_type')
+    categories = db.relationship('Category', secondary=camera_categories, backref='camera_categories', lazy='dynamic')
     city = db.Column(db.String(64), index=True)
     country = db.Column(db.String(64), index=True)
     latitude_n = db.Column(db.Numeric(precision=10, scale=8))
