34d75e2019216873516bac9e1796b745832caf2c
dgeene <david.geene@gmail.com>
2016-08-06 11:33:27 -0400

    schema has been completed. Need to verify relationships behave the way i intend

--- app/models.py
+++ app/models.py
@@ -1,13 +1,22 @@
 from app import db
-
-class Cameras(db.Model):
-    pass
+# this schema needs to be verified
+# are bridge tables being created?
+# http://docs.sqlalchemy.org/en/rel_1_0/orm/basic_relationships.html#many-to-many
+#
 
 class Category(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(20), index=True)
+
+    def __repr__(self):
+        return '<Category %r>' % (self.category_name)
 
 class CameraType(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(32), index=True)
+
+    def __repr__(self):
+        return '<Type %r>' % (self.type)
 
 
 class User(db.Model):
@@ -17,4 +26,20 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
+class Cameras(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    camera_name = db.Column(db.String(255), index=True)
+    url = db.Column(db.String(255))
+    type = db.relationship('CameraType', backref='camera_type', lazy='dynamic')
+    categories = db.relationship('Category', backref='camera_categories', lazy='dynamic')
+    city = db.Column(db.String(64), index=True)
+    country = db.Column(db.String(64), index=True)
+    latitude_n = db.Column(db.Numeric(precision=10, scale=8))
+    latitude_e = db.Column(db.Numeric(precision=10, scale=8))
+    coords_verified = db.Column(db.Integer)
+    comment = db.Column(db.String(255))
+
+    def __repr__(self):
+        return '<CameraName %r>' % (self.camera_name)
 
